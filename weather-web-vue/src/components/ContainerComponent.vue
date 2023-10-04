7f1ad06dfb4ea5bb4dcd13ae155995e297be1d6a
NunuuuAP <nunuap@gmail.com>
2023-10-05 01:31:06 +0200

    feat: add Container as component

---
+++ weather-web-vue/src/components/ContainerComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.container {
+	display: flex;
+	flex: 1;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+	background-color: var(--color-background-soft);
+	color: var(--color-text);
+	width: 100%;
+	border-radius: 25px;
+	padding: 30px;
+    overflow: auto;
+}
+
+</style>
