7b603ed45fbf5db5a6514edd629908994e814b59
NunuuuAP <nunuap@gmail.com>
2023-10-09 13:43:43 +0200

    fix(router): add subscription route to avoid errors

--- weather-web-vue/src/router/index.ts
+++ weather-web-vue/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import HourView from '@/views/HourView.vue'
 import WeekView from '@/views/WeekView.vue'
+import SubscriptionView from '@/views/SubscriptionView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,12 @@ const router = createRouter({
       path: '/weekly',
       name: 'weekly',
       component: WeekView
+    }
+    ,
+    {
+      path: '/subscription',
+      name: 'subscription',
+      component: SubscriptionView
     }
   ]
 })
